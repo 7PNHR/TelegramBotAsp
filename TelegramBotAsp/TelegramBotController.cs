bbb8ebbb2610923d26b9890bd8398488258463a5
LPNHR <lpnhr.style@gmail.com>
2021-11-10 18:42:51 +0500

    FC

---
+++ TelegramBotAsp/TelegramBotController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace TelegramBotAsp
+{
+    [ApiController]
+    [Route("api/message")]
+    public class TelegramBotController : ControllerBase
+    {
+        // GET
+        [HttpPost("update")]
+        public IActionResult Update(Update update)
+        {
+            return Ok();
+        }
+    }
+}
