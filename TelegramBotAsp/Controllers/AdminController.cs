641f519b4056d6bdaa9a85c9c193f122ca7b5bee
LPNHR <lpnhr.style@gmail.com>
2021-12-01 23:49:56 +0500

    Log added fix

--- TelegramBotAsp/Controllers/AdminController.cs
+++ TelegramBotAsp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -21,6 +22,19 @@ namespace TelegramBotAsp.Controllers
         public async Task<IActionResult> GetTemplates()
         {
             return Ok(JsonConvert.SerializeObject(_context.Templates.ToList()));
+        }
+        
+        [HttpGet("bot-logs")]
+        public async Task<IActionResult> GetBotLogs()
+        {
+            return Ok(JsonConvert.SerializeObject(_context.Logs.ToList()));
+        }
+        
+        [HttpGet("bot-logs-period")]
+        public async Task<IActionResult> GetBotLogs(int days)
+        {
+            var date = DateTime.UtcNow - TimeSpan.FromDays(days);
+            return Ok(JsonConvert.SerializeObject(_context.Logs.Where(x=>x.Date >= date).ToList()));
         }
         
         [HttpPost("create-template")]
